2cd89484bb3829a399781c8915450b00e0e2a32f
George Spanos <gespa11019@gmail.com>
2021-02-06 02:45:57 +0200

    Init complete

--- helpers/initialize.py
+++ helpers/initialize.py
@@ -1,5 +1,8 @@
+from pizza import Pizza
+
+
 def initialize():
-    input = open("a_example", "r")
+    input = open("a_example.txt", "r")
     text = input.read()
     input.close()
 
@@ -8,21 +11,27 @@ def initialize():
         first_split[i] = first_split[i].strip()
 
     first_split = list(filter(None, first_split))
-    # print(first_split)
 
     for i, y in zip(first_split, range(len(first_split))):
         first_split[y] = first_split[y].split(" ")
-    # print(first_split)
 
     first_row = first_split[0]
 
     total_pizzas = first_split[0][0]
     teams = {
-        "two": first_row[0],
-        "three": first_row[1],
-        "four": first_row[2]
+        "two": first_row[1],
+        "three": first_row[2],
+        "four": first_row[3]
     }
     print("Teams: ", teams)
     print("Total pizzas: ", total_pizzas)
-
+    first_split.pop(0)
+    pizzas = []
+    for _pizza, index in zip(first_split, range(len(first_split))):
+        _pizza.pop(0)
+        ingredients = _pizza
+        pizza = Pizza(index, _pizza)
+        pizzas.append(pizza)
+    for pizza in pizzas:
+        print(pizza)
 __name__ == 'initialize'
--- pizza.py
+++ pizza.py
@@ -1,4 +1,9 @@
 class Pizza:
-    def __init__(self, id, ingredients):
-       self.id = id
-       self.ingredients = ingredients
+    def __init__(self, Id, ingredients):
+        self.Id = Id
+        self.ingredients = ingredients
+        self.sent = False
+        self.ingredients_lenth = len(self.ingredients)
+
+    def __str__(self):
+        return f"Id: {self.Id}, Ingredients: {self.ingredients}, Sent: {self.sent}"
