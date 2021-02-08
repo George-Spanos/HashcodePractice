3ca218cda8df5ffc3d8046bb88124e733debdfaf
George Spanos <gespa11019@gmail.com>
2021-02-08 18:44:43 +0200

    elias 1

--- app.py
+++ app.py
@@ -7,29 +7,39 @@ from get_combination import get_combination
 
 import time
 
-start = time.time()
-teams, pizzas, total_pizzas = initialize()
 
-combination = get_combination(total_pizzas, teams)
-print(combination)
-pizzas_sl = create_similarity_levels(pizzas)
+def calc_delivery():
+    start = time.time()
+    teams, pizzas, total_pizzas = initialize()
 
-print(pizzas_sl)
+    combination = get_combination(total_pizzas, teams)
+    print(combination)
+    pizzas_sl = create_similarity_levels(pizzas)
 
-delivery_notes = Delivery_Notes()
-for noOfMembers in combination:
-    delivery_notes.add_team(Team(noOfMembers))
-for pizza_id in pizzas_sl.keys():
-    pizza = [pizza for pizza in pizzas if pizza.Id == pizza_id][0]
-    delivery_notes.add_pizza(pizza)
-delivery_notes.calc_total_score()
-file = open("res.txt", "w")
-file.write(f"{delivery_notes.total_deliveries}\n")
-for team in delivery_notes.teams:
-    file.write(f"{team.members} ")
-    for pizza in team.pizzas:
-        file.write(f"{pizza.Id} ")
-    file.write("\n")
-end = time.time()
-print(end - start)
-print(delivery_notes.total_score)
+    print(pizzas_sl)
+
+    delivery_notes = Delivery_Notes()
+    for noOfMembers in combination:
+        delivery_notes.add_team(Team(noOfMembers))
+    for pizza_id in pizzas_sl.keys():
+        pizza = [pizza for pizza in pizzas if pizza.Id == pizza_id][0]
+        delivery_notes.add_pizza(pizza)
+    delivery_notes.calc_total_score()
+    file = open("res.txt", "w")
+    file.write(f"{delivery_notes.total_deliveries}\n")
+    for team in delivery_notes.teams:
+        file.write(f"{team.members} ")
+        for pizza in team.pizzas:
+            file.write(f"{pizza.Id} ")
+        file.write("\n")
+    end = time.time()
+    print(end - start)
+    print(delivery_notes.total_score)
+
+
+def main():
+    calc_delivery()
+
+
+if __name__ == "__main__":
+    main()
